5f794e96579020942b7784a2a06bf838bc54a656
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-18 16:22:07 +0200

    adds NA option

--- relations.py
+++ relations.py
@@ -1,10 +1,23 @@
 from sklearn.metrics import matthews_corrcoef
 import data
+import utils
 
 endpoints_data = data.get_endpoints_data()
 
-y_true = endpoints_data["linguistic"]["AmorphousURI"]
-y_pred = endpoints_data["design"]["isMisusingCookies"]
 
-result = matthews_corrcoef(y_true, y_pred)
-print(result)
+def stats(type_a, type_b):  # type_a & type_b: antipattern types
+    for a_key in endpoints_data[type_a].keys():
+        y_true_list = endpoints_data[type_a][a_key]
+
+        for b_key in endpoints_data[type_b].keys():
+            y_pred_list = endpoints_data[type_b][b_key]
+
+            if (utils.all_zero(y_true_list) or utils.all_zero(y_pred_list)):
+                print(a_key, "vs", b_key, "NA")
+            else:
+                result = matthews_corrcoef(y_true_list, y_pred_list)
+                print(a_key, "vs", b_key, result)
+
+
+stats("linguistic", "design")
+stats("design", "linguistic")
