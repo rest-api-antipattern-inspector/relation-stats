6340daaa648744d995d2c9408fa2f6a21a15ec2b
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-18 14:51:05 +0200

    it works, same result

--- relations.py
+++ relations.py
@@ -1,5 +1,10 @@
+from sklearn.metrics import matthews_corrcoef
 import data
 
 endpoints_data = data.get_endpoints_data()
 
-print(endpoints_data)
+y_true = endpoints_data["linguistic"]["AmorphousURI"]
+y_pred = endpoints_data["design"]["isMisusingCookies"]
+
+result = matthews_corrcoef(y_true, y_pred)
+print(result)
